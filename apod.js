1b2034533e4f59ccdaae70b34367571dae19737f
alumno <alumno@Instituto.local>
2025-10-01 08:23:50 -0300

    .

--- apod.js
+++ apod.js
@@ -1,4 +1,43 @@
-fetch("https://api.themoviedb.org/3/tv/1395?api_key=432f5d21e0d2cd45a85806851125e8bc")
+
+let peliculaElegida = document.getElementById ('selector').value
+
+
+fetch(" https://api.themoviedb.org/3/query=${encodeURIComponent(selector)}&api_key=432f5d21e0d2cd45a85806851125e8bc")
+
+.then(res => res.json())
+.then(datos => {
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fetch("https://api.themoviedb.org/3/tv/126390?api_key=432f5d21e0d2cd45a85806851125e8bc")
   .then(res => res.json())
 
   
