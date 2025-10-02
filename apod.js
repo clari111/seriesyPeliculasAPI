4cbc380ef573197b2c3bb301143c597c6a2ac34b
alumno <alumno@Instituto.local>
2025-10-02 11:24:34 -0300

    .

--- apod.js
+++ apod.js
@@ -1,43 +1,22 @@
+let peliculaElegida = document.getElementById('selector').value;
 
-let peliculaElegida = document.getElementById ('selector').value
-
-
-fetch(" https://api.themoviedb.org/3/query=${encodeURIComponent(selector)}&api_key=432f5d21e0d2cd45a85806851125e8bc")
-
-.then(res => res.json())
-.then(datos => {
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fetch("https://api.themoviedb.org/3/tv/126390?api_key=432f5d21e0d2cd45a85806851125e8bc")
+fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(peliculaElegida)}&api_key=432f5d21e0d2cd45a85806851125e8bc`)
   .then(res => res.json())
+  .then(datos => {
+    let resultados = datos.results;
+    if (resultados.length > 0) {
+      let id = resultados[0].id;
+      
+      return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=432f5d21e0d2cd45a85806851125e8bc`);
+    } else {
+      alert("No se encontró la película");
 
-  
+    }
+  })
+  .then(res => res.json())
+  .then(pelicula => {
+    console.log("Título:", pelicula.title);
+    console.log("Descripción:", pelicula.overview);
+    console.log("Fecha de estreno:", pelicula.release_date);
+  })
+
