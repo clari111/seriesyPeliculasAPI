bce397452b9ecf1faa0405efc14706ac0a2fd8f9
claradehuevo <136344430+clari111@users.noreply.github.com>
2025-10-07 11:04:54 -0300

    Actualizar apod.js

--- apod.js
+++ apod.js
@@ -1,22 +1,31 @@
-let peliculaElegida = document.getElementById('selector').value;
 
-fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(peliculaElegida)}&api_key=432f5d21e0d2cd45a85806851125e8bc`)
-  .then(res => res.json())
-  .then(datos => {
-    let resultados = datos.results;
-    if (resultados.length > 0) {
-      let id = resultados[0].id;
+
+
+
+let key = "432f5d21e0d2cd45a85806851125e8bc"
+
+
+  let descripcionPeli = document.querySelector('#descripcionPeli')
+let boton = document.querySelector('#boton')
+let titulo = document.querySelector('h1')
+
+fetch ( 'https://api.themoviedb.org/3/movie/343611&api_key=432f5d21e0d2cd45a85806851125e8bc')
+ 
+
+
+boton.onclick = function () {
+  
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+    .then(res => res.json())
+    .then(peliculaInfo => {
       
-      return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=432f5d21e0d2cd45a85806851125e8bc`);
-    } else {
-      alert("No se encontró la película");
+      descripcionPeli.src = peliculaInfo.overview
+      titulo.textContent = peliculaInfo.title
+    })
+}
 
-    }
-  })
-  .then(res => res.json())
-  .then(pelicula => {
-    console.log("Título:", pelicula.title);
-    console.log("Descripción:", pelicula.overview);
-    console.log("Fecha de estreno:", pelicula.release_date);
-  })
+
+
+
+
 
