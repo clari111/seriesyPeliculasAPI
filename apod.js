8c1a0b6ec6cd7a6d78c718048bb245542f79fb6b
alumno <alumno@Instituto.local>
2025-10-08 08:26:03 -0300

    .

--- apod.js
+++ apod.js
@@ -1,28 +1,30 @@
 
+let key = "432f5d21e0d2cd45a85806851125e8bc";
+
+let descripcionPeli = document.querySelector('#descripcionPeli');
+let boton = document.querySelector('#boton');
+let titulo = document.querySelector('#titulo'); 
 
 
 
-let key = "432f5d21e0d2cd45a85806851125e8bc"
 
-
-  let descripcionPeli = document.querySelector('#descripcionPeli')
-let boton = document.querySelector('#boton')
-let titulo = document.querySelector('h1')
-
-fetch ( 'https://api.themoviedb.org/3/movie/343611&api_key=432f5d21e0d2cd45a85806851125e8bc')
- 
-
+let movieId = 11631 ;
 
 boton.onclick = function () {
-  
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+  fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}&language=es`)
     .then(res => res.json())
     .then(peliculaInfo => {
-      
-      descripcionPeli.src = peliculaInfo.overview
-      titulo.textContent = peliculaInfo.title
+      // Mostramos la info en la página
+      descripcionPeli.textContent = peliculaInfo.overview;
+      titulo.textContent = peliculaInfo.title;
     })
+    .catch(error => {
+      alert('Error al obtener los datos de la película:', error);
+    });
 }
+
+
+
 
 
 
